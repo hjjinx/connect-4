f54e896bcf533b8cf45a44292c0bfa9eef6833e4
Himanshu Jain <hjmanojain.8@gmail.com>
2019-05-18 14:40:29 +0530

    Updates

---
+++ precache-manifest.8ae1f9c3322b95686531fe531191439b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e72e6944edf12af62cb59d4f5d174d9",
+    "url": "/connect-4/index.html"
+  },
+  {
+    "revision": "5544a8a6e83e074edd41",
+    "url": "/connect-4/static/css/main.f067fd1f.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/connect-4/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "5544a8a6e83e074edd41",
+    "url": "/connect-4/static/js/main.eaee0696.chunk.js"
+  },
+  {
+    "revision": "988652424af3b2d832e7",
+    "url": "/connect-4/static/js/runtime~main.308217db.js"
+  }
+]);
